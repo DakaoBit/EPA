f201e6dc3c0071ef7c084370312d8686183a01dc
DakaoBit <oasis.wang1016@gmail.com>
2019-09-18 18:11:02 +0800

    DakaoBit 2019/9/18 - Build EPA : 空氣品質指標(AQI)

--- EPA/Controllers/HomeController.cs
+++ EPA/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EPA.Models.ViewModels;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +11,67 @@ namespace EPA.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string County)
         {
-            return View();
+            var client = new RestClient("http://opendata2.epa.gov.tw/AQI.json");
+            var request = new RestRequest(Method.GET);
+            //request.AddParameter("skip", "0", ParameterType.RequestBody);
+            //request.AddParameter("top", "1000", ParameterType.RequestBody);
+            //request.AddParameter("format", "json", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            var AQI = JsonConvert.DeserializeObject<IEnumerable<AQIViewModel>>(response.Content);
+
+            //SelectListItem
+            var county = AQI
+                .GroupBy(o => new { countyName = o.County })
+                .Select(c => c.Key.countyName).ToList();
+
+            var countyOptions = new List<SelectListItem>();
+            countyOptions.AddRange(county.ConvertAll(o => {
+                return new SelectListItem()
+                {
+                    Text = o.ToString(),
+                    Value = o.ToString(),
+                };
+            }));
+
+            ViewBag.countyOptions = countyOptions;
+
+            //data
+            if (County != null)
+                AQI = AQI.Where(o => o.County == County);
+            else
+                AQI = AQI.Where(o => o.County == "基隆市");
+
+            return View(AQI);
+
+        }
+ 
+        /// <summary>
+        /// 取得單一監測站資料
+        /// </summary>
+        /// <param name="Site"></param>
+        /// <returns></returns>
+        public JsonResult GetSite(string SiteId)
+        {
+            AQIViewModel site = new AQIViewModel();
+
+            var client = new RestClient("http://opendata2.epa.gov.tw/AQI.json");
+            var request = new RestRequest(Method.GET);
+            //request.AddParameter("skip", "0", ParameterType.RequestBody);
+            //request.AddParameter("top", "1000", ParameterType.RequestBody);
+            //request.AddParameter("format", "json", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            var AQI = JsonConvert.DeserializeObject<IEnumerable<AQIViewModel>>(response.Content);
+
+            if (SiteId != null)
+                site = AQI.Where(o => o.SiteId == SiteId).FirstOrDefault();
+
+            return Json(site, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
